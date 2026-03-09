a35f3ded75e22051eeec09971e531d3651549ca3
philgweber <philweber@microsoft.com>
2026-03-09 16:47:11 -0700

    Move platform code for secure-services
    Move patina-qemu to subfolder and clean up Makefile deps and devcontainer.

---
+++ secure-services/platform/src/main.rs
@@ -0,0 +1,35 @@
+// This project is dual-licensed under Apache 2.0 and MIT terms.
+// See LICENSE-APACHE and LICENSE-MIT for details.
+
+#![cfg_attr(target_os = "none", no_std)]
+#![cfg_attr(target_os = "none", no_main)]
+#![deny(clippy::undocumented_unsafe_blocks)]
+#![deny(unsafe_op_in_unsafe_fn)]
+
+#[cfg(target_os = "none")]
+mod baremetal;
+
+#[cfg(not(target_os = "none"))]
+fn main() {
+    println!("qemu-sp stub");
+}
+
+#[cfg(target_os = "none")]
+fn main() -> ! {
+    use ec_service_lib::MessageHandler;
+    use odp_ffa::Function;
+
+    log::info!("QEMU Secure Partition - build time: {}", env!("BUILD_TIME"));
+
+    let version = odp_ffa::Version::new().exec().unwrap();
+    log::info!("FFA version: {}.{}", version.major(), version.minor());
+
+    MessageHandler::new()
+        .append(ec_service_lib::services::Thermal::new())
+        .append(ec_service_lib::services::FwMgmt::new())
+        .append(ec_service_lib::services::Notify::new())
+        .run_message_loop()
+        .expect("Error in run_message_loop");
+
+    unreachable!()
+}
